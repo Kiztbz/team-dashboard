156d4625698e12e2df07b09a51d466bb6fe01fa9
kiztbz <pragyapravita@gmail.com>
2026-02-16 01:22:56 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-import Login from "./pages/Login";
-import Owner from "./pages/Owner";
-import Admin from "./pages/Admin";
-import Team from "./pages/Team";
-import Client from "./pages/Client";
+import Login from "./pages/Login.jsx";
+import Owner from "./pages/Owner.jsx";
+import Admin from "./pages/Admin.jsx";
+import Team from "./pages/Team.jsx";
+import Client from "./pages/Client.jsx";
 
 export default function App() {
     const [user, setUser] = useState(null);
